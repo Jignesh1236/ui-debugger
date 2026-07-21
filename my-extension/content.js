f31c83b158e5d27808ce08def90215c30e21ce18
jignesh1236 <jigneshmaru690@gmail.com>
2026-07-21 13:01:20 +0530

    hover elements changes

--- my-extension/content.js
+++ my-extension/content.js
@@ -1,32 +1,19 @@
 console.log("Content script loaded");
-const overlay = document.createElement("div");
-overlay.innerHTML = `hello`;
-
-
 
 function onOff() {
     document.body.classList.toggle("ui-debugger");
     console.log("Toggled");
     document.body.classList.contains("ui-debugger") ? console.log("on") : console.log("off");
-
-    if (document.body.classList.contains("ui-debugger")) {
-        overlay.style.display = "block";
-        document.addEventListener("mousemove", moveOverlay);
-    } else {
-        overlay.style.display = "none";
-        document.removeEventListener("mousemove", moveOverlay);
-    }
+    // show width and height of element on hover
+    document.body.addEventListener("mouseover", (event) => {
+        if (document.body.classList.contains("ui-debugger")) {
+            const element = event.target;
+            const width = element.offsetWidth;
+            const height = element.offsetHeight;
+            console.log(`Width: ${width}px, Height: ${height}px`);
+        }
+    });
 }
-
-function moveOverlay(e) {
-    const rect = e.target.getBoundingClientRect();
-
-    overlay.style.left = rect.left + "px";
-    overlay.style.top = rect.top + "px";
-    overlay.style.width = rect.width + "px";
-    overlay.style.height = rect.height + "px";
-}
-
 
 chrome.runtime.onMessage.addListener((message) => {
     if (message.action === "toggleDebugger") {
