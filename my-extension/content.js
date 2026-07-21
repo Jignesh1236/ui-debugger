482856f7de47d7f4a8da64b6dc70c267b5d58683
jignesh1236 <jigneshmaru690@gmail.com>
2026-07-21 12:42:11 +0530

    on off toggle

--- my-extension/content.js
+++ my-extension/content.js
@@ -3,6 +3,7 @@ console.log("Content script loaded");
 function onOff() {
     document.body.classList.toggle("ui-debugger");
     console.log("Toggled");
+    document.body.classList.contains("ui-debugger") ? console.log("on") : console.log("off");
 }
 
 chrome.runtime.onMessage.addListener((message) => {
