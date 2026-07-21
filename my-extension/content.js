73d8e58a227a1f11c3798d08356bc6d2a090e29a
jignesh1236 <jigneshmaru690@gmail.com>
2026-07-21 13:14:21 +0530

    width and height

--- my-extension/content.js
+++ my-extension/content.js
@@ -1,17 +1,20 @@
 console.log("Content script loaded");
-
+const overlay = document.createElement("div");
+overlay.id = "width-height";
 function onOff() {
     document.body.classList.toggle("ui-debugger");
     console.log("Toggled");
     document.body.classList.contains("ui-debugger") ? console.log("on") : console.log("off");
-    // show width and height of element on hover
-    document.body.addEventListener("mouseover", (event) => {
-        if (document.body.classList.contains("ui-debugger")) {
-            const element = event.target;
-            const width = element.offsetWidth;
-            const height = element.offsetHeight;
-            console.log(`Width: ${width}px, Height: ${height}px`);
-        }
+    // show width and height of element on hover toggle disply hidden and visible useing css #width-height
+    let elements = document.querySelectorAll("*");
+    elements.forEach((element) => {
+        element.addEventListener("mouseover", (event) => {
+            if (document.body.classList.contains("ui-debugger")) {
+                const width = event.target.offsetWidth;
+                const height = event.target.offsetHeight;
+                overlay.textContent = `Width: ${width}px, Height: ${height}px`;
+            }
+        });
     });
 }
 
