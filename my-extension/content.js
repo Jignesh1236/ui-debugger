84302e8372bbfd76d473f9c949eb4c7d648df70d
jignesh1236 <jigneshmaru690@gmail.com>
2026-07-21 12:59:23 +0530

    ongoing changes

--- my-extension/content.js
+++ my-extension/content.js
@@ -1,10 +1,32 @@
 console.log("Content script loaded");
+const overlay = document.createElement("div");
+overlay.innerHTML = `hello`;
+
+
 
 function onOff() {
     document.body.classList.toggle("ui-debugger");
     console.log("Toggled");
     document.body.classList.contains("ui-debugger") ? console.log("on") : console.log("off");
+
+    if (document.body.classList.contains("ui-debugger")) {
+        overlay.style.display = "block";
+        document.addEventListener("mousemove", moveOverlay);
+    } else {
+        overlay.style.display = "none";
+        document.removeEventListener("mousemove", moveOverlay);
+    }
 }
+
+function moveOverlay(e) {
+    const rect = e.target.getBoundingClientRect();
+
+    overlay.style.left = rect.left + "px";
+    overlay.style.top = rect.top + "px";
+    overlay.style.width = rect.width + "px";
+    overlay.style.height = rect.height + "px";
+}
+
 
 chrome.runtime.onMessage.addListener((message) => {
     if (message.action === "toggleDebugger") {
